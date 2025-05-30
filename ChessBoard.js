399c4b874c3287445bc097fb144ab829f7106261
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-30 20:31:49 +0100

    Chess Board using JavaScript

---
+++ ChessBoard.js
@@ -0,0 +1,12 @@
+let output = ""
+let nxtPrint = ""
+for (index = 0; index < 4; index++){
+	for (count = 0; count < 4; count++){
+	output += "# "
+	nxtPrint += " #"
+	}
+	console.log(output)
+	console.log(nxtPrint)
+	output = ""
+	nxtPrint = ""
+}
