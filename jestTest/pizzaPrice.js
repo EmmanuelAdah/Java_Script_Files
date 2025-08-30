d04b6bf01b19684f838994c41418306c265e9761
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-30 19:53:43 +0100

    finding pizza price from a list of pizza

---
+++ jestTest/pizzaPrice.js
@@ -0,0 +1,9 @@
+function pizzaPriceAccumulator({pizza : amount}){
+    let totalPrice = 0
+    let pizza = [{"pepperoni" : 7000},{"veggie":10000}, {"chickenSuya" : 15000}]
+
+
+}
+
+
+module.exports = {pizzaPriceAccumulator}
