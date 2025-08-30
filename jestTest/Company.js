b6cfb4473e2d090f05a4912ad0cf95622c727e6a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-08-30 19:43:15 +0100

    Returning the details of employees in a company

---
+++ jestTest/Company.js
@@ -0,0 +1,11 @@
+const company = function(){
+    name: "TechCorp"
+    location: "San Francisco"
+    employees: ["John", "James", "Emma", "Theo"]
+    id: "H21"
+    department: ["Engineering", "HR", "Marketing"]
+}
+
+module.exports = {
+    company
+}
